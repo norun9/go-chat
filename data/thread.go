dbff00fa1e079507f58a9122fd7401a0f31c0d68
tomoyaueno29 <u.t012807@gmail.com>
2020-06-04 13:24:50 +0900

    Update thread.go

--- data/thread.go
+++ data/thread.go
@@ -11,3 +11,10 @@ type Thread struct {
 	UserId    int
 	CreatedAt time.Time
 }
+
+func Tread() (threads []Tread, err error){
+	rows, err := Db.Query("SELECT count(*) FROM posts where thread_id = $1", thread.Id)
+	if err != nil{
+		return
+	}
+}
