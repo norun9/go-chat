e135770585d00eeca6b258a49eb259d9e9ef3bfa
tomoyaueno29 <u.t012807@gmail.com>
2020-06-04 12:39:34 +0900

    Update util.go

--- util.go
+++ util.go
@@ -23,3 +23,14 @@ func session(w http.ResponseWriter, r *http.Request)(sess data.Session, err erro
 	}
 	return
 }
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string){
+	var files []string
+	for _, file := range filenames{
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}{
+
+}
