87c5fd1ef308e3e0bd0460643301ac542cb90ef2
tomoyaueno29 <u.t012807@gmail.com>
2020-06-04 12:01:34 +0900

    Update util.go

--- util.go
+++ util.go
@@ -1,3 +1,16 @@
 package main
 
-func 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	// 	"github.com/sausheong/gwp/Chapter_2_Go_ChitChat/chitchat/data"
+	"github.com/mushahiroyuki/gowebprog/ch02/chitchat/data"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func session
