c3e5c168bed16689c9dee11ad90f3a52b2a7cdd1
tomoyaueno29 <u.t012807@gmail.com>
2020-06-04 12:46:16 +0900

    Update main.go

--- main.go
+++ main.go
@@ -5,26 +5,37 @@ import(
 	"html/template"
 )
 
+//func index(w http.ResponseWriter, r *http.Request){
+//	threads, err := data.Thread(); if err == nil {
+//		_, err := session(w, r)
+//		public_tmpl_files := []string{
+//			"templates/layout,html",
+//			"templates/public.navbar.html",
+//			"templates/index.html",
+//		}
+//		private_tmpl_files := []string{
+//			"templates/layout,html",
+//			"templates/private.navbar.html",
+//			"templates/index.html",
+//		}
+//		var templates *template.Template
+//		if err != nil {
+//			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+//		} else {
+//			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+//		}
+//		templates.ExecuteTemplate(w, "layouts", threads)
+//	}
+//}
+
 func index(w http.ResponseWriter, r *http.Request){
-	threads, err := data.Thread(); if err == nil {
+	threads, err := data.Treads(); if err == nil {
 		_, err := session(w, r)
-		public_tmpl_files := []string{
-			"templates/layout,html",
-			"templates/public.navbar.html",
-			"templates/index.html",
-		}
-		private_tmpl_files := []string{
-			"templates/layout,html",
-			"templates/private.navbar.html",
-			"templates/index.html",
-		}
-		var templates *template.Template
 		if err != nil {
-			templates = template.Must(template.ParseFiles(private_tmpl_files...))
-		} else {
-			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+			generateHTML(w, threads, "layout", "public.navbar", "index")
+		}else{
+			generateHTML(w, threads, "layout", "private.navbar", "index")
 		}
-		templates.ExecuteTemplate(w, "layouts", threads)
 	}
 }
 
