eb415aecdc5ca3766cd388e3197a189108e18168
tomoyaueno29 <u.t012807@gmail.com>
2020-06-04 12:23:36 +0900

    Update main.go

--- main.go
+++ main.go
@@ -6,13 +6,24 @@ import(
 )
 
 func index(w http.ResponseWriter, r *http.Request){
-	files := []string{
-		"templates/layout,html",
-		"templates/navbar.html",
-		"templates/index.html",
-	}
-	templates := template.Must(template.ParseFiles(files...))
-	threads, err := data.Thread(); if err != nil {
+	threads, err := data.Thread(); if err == nil {
+		_, err := session(w, r)
+		public_tmpl_files := []string{
+			"templates/layout,html",
+			"templates/public.navbar.html",
+			"templates/index.html",
+		}
+		private_tmpl_files := []string{
+			"templates/layout,html",
+			"templates/private.navbar.html",
+			"templates/index.html",
+		}
+		var templates *template.Template
+		if err != nil {
+			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+		} else {
+			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+		}
 		templates.ExecuteTemplate(w, "layouts", threads)
 	}
 }
