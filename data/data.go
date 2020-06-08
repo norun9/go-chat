a0dfc01ca9d8574d572651be5964ed2b70a61a87
tomoyaueno29 <u.t012807@gmail.com>
2020-06-08 12:58:17 +0900

    Update data.go

--- data/data.go
+++ data/data.go
@@ -1,27 +1,17 @@
 package data
 
-import(
+import (
 	"database/sql"
 	"log"
-	"time"
 )
 
 var Db *sql.DB
 
 func init() {
 	var err error
-	Db, err = sql.Open("postgres", "dbname=gochat sslmode=disable")
+	Db, err = sql.Open("postgres", "username=tomoyaueno dbname=gochat sslmode=disable")
 	if err != nil{
 		log.Fatal(err)
 	}
 	return
 }
-
-type Tread struct {
-	Id int
-	Uuid string
-	Topic string
-	UserId string
-	CreatedAt time.Time
-}
-
